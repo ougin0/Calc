d93b5c991b4dc936abba2f281fc3ed9656005670
fasolksa <ftyy2015@hotmail.com>
2024-10-19 17:16:17 +0300

    Initial commit

---
+++ Calc/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Choose an operation (+, -, *, /): ");
+        string operation = Console.ReadLine();
+
+        double result = 0;
+        bool validOperation = true;
+
+        switch (operation)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Error! Division by zero.");
+                    validOperation = false;
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid operation.");
+                validOperation = false;
+                break;
+        }
+
+        if (validOperation)
+        {
+            Console.WriteLine($"Result: {num1} {operation} {num2} = {result}");
+        }
+    }
+}
